2f988eaa7e8440cff25fdb35d5f0419225233923
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-11-07 23:33:06 +0530

    add listing item details page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -113,7 +113,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
     // Store image in firebase
@@ -175,7 +174,6 @@ function CreateListing() {
     formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, "listings"), formDataCopy);
