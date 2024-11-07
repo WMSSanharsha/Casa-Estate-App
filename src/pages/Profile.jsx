b5ad361ac9af8f2b6f92b30bf390ec4a502f9711
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-11-07 17:12:35 +0530

    update profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,33 @@
 import { getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
   const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-  }, []);
+  const { name, email } = formData;
 
-  return user ? <h1>{user.displayName}</h1> : "Not Logged In";
+  const navigate = useNavigate();
+
+  function onLogout() {
+    auth.signOut();
+    navigate("/");
+  }
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Log Out
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
